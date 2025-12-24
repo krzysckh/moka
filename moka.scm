1d5ae64ccf1afe2d4d56001e8294829d03ad6704
Krzysztof Michałczyk <kpm@krzysckh.org>
2025-12-24 19:02:01 +0100

    add more routes

--- moka.scm
+++ moka.scm
@@ -2,6 +2,7 @@
 
 (import
  (owl toplevel)
+ (owl unicode)
  (prefix (owl sys) sys/)
  (prefix (robusta fastcgi) r/)
  (prefix (robusta log) r/)
@@ -17,6 +18,17 @@
 
 (import
  (prefix (ext sqlite io) s3/))
+
+(define (execute* ptr s arg)
+  (map
+   (λ (l)
+     (map
+      (λ (v)
+        (if (string? v)
+            (bytes->string (utf8-decode (string->bytes v)))
+            v))
+      l))
+   (s3/execute ptr s arg)))
 
 (define *db-file* "moka.db")
 (define *uploads-dir* "static/uploads")
@@ -45,7 +57,7 @@
 
 ;; unsafe
 (define (table-has-column? ptr name col)
-  (let ((v (s3/execute ptr (str "SELECT * FROM pragma_table_info('" name "') WHERE name = ?") (list (str col)))))
+  (let ((v (execute* ptr (str "SELECT * FROM pragma_table_info('" name "') WHERE name = ?") (list (str col)))))
     (> (len v) 0)))
 
 (define (list->sql-list lst)
@@ -120,6 +132,11 @@
 (define (db)
   (interact 'db (tuple 'get)))
 
+(define-table (uploads make-upload)
+  timestamp => datetime
+  location  => text
+  )
+
 (define-table (roasteries make-roastery)
   name   => text
   image  => int
@@ -127,42 +144,47 @@
   notes  => text
   )
 
+(define-table (methods make-method)
+  name  => text
+  image => int                    ; -> uploads
+  notes => text
+  )
+
 (define-table (coffees make-coffee)
   name        => text
-  roastery    => int
+  roastery    => int                    ; -> roasteries
   roast_level => int
-  image       => int    ; -> uploads
+  image       => int                    ; -> uploads
   url         => text
-  additional  => text
+  notes       => text
   )
 
 (define-table (grinders make-grinder)
-  name        => text
-  image       => int
-  url         => text
-  additional  => text
+  name  => text
+  image => int                    ; -> uploads
+  url   => text
+  notes => text
   )
 
-(define-table (methods make-method)
-  name        => text
-  image       => int
-  additional  => text
+(define-table (gear make-gear)          ; espresso machines, moka pots et al
+  name  => text
+  url   => text
+  image => int  ; -> uploads
+  notes => text
   )
 
-(define-table (uploads make-upload)
-  timestamp => datetime
-  location  => text
-  )
-
-(define-table (opinions make-opinion)
+(define-table (brews make-brew)
   timestamp    => datetime
   coffee       => int  ; -> coffees
   grinder      => int  ; -> grinders
   method       => int  ; -> methods
+  gear         => int  ; -> gear
   local_p      => bool ; made at home?
   grind_level  => int
   rating       => int
   image        => int  ; -> uploads
+  dose         => int  ; coffee dose (grams)
+  yield        => int  ; coffee yield (grams)
   notes        => text
   )
 
@@ -172,7 +194,17 @@
 
 (define (db-get table items)
   (lets ((p (db)))
-    (s3/execute p (str "SELECT " (list->sql-list items) " FROM " table) #n)))
+    (execute* p (str "SELECT " (list->sql-list items) " FROM " table) #n)))
+
+(define known-routes
+  `(("opinia"   "coffee"       "/brews")
+    ("obrazki"  "image"        "/uploads")
+    ("palarnie" "mode_heat"    "/roasteries")
+    ("metody"   "procedure"    "/methods")
+    ("kawki"    "local_cafe"   "/coffees")
+    ("młynki"   "cyclone"      "/grinders")
+    ("machiny"  "coffee_maker" "/gear")
+    ))
 
 (define (make-page body)
   (html/encode
@@ -185,40 +217,69 @@
       ((script (type . "module") (src . "https://cdn.jsdelivr.net/npm/beercss@3.13.1/dist/cdn/beer.min.js")))
       ((script (type . "module") (src . "https://cdn.jsdelivr.net/npm/material-dynamic-colors@1.1.2/dist/cdn/material-dynamic-colors.min.js"))))
      ((body (class . "dark"))
+      ((nav (class . "m l left max"))
+       ;;; TODO: header
+       ;; (header
+       ;;  (h3 "moka"))
+       ,@(map
+          (λ (it)
+            `((a (href . ,(caddr it)))
+              (i ,(cadr it))
+              (span ,(car it))))
+          known-routes))
       ((main (class . "responsive"))
-       ((dialog (id . "choose_upload_dialog"))
-        (h5 "wybierz obrazek")
-        (article
-         ,@(map (λ (it)
-                  ;; `((img (src . ,(str (car it))) (loading . "lazy"))))
-                  `((article (class . "no-padding border"))
-                    ((img (class . "responsive medium") (src . ,(str (car it))) (loading . "lazy")))
-                    ((div (class . "absolute bottom left right padding bottom-shadow white-text"))
-                     (nav
-                      ((button (class . "circle transparent") (type . "button") (onClick . ,(str "image_selected('" (str (cadr it)) "')")))
-                       (i "add"))))))
-                (db-get 'uploads '(location id)))))
-       ,@body)))))
+       ,@body)
+      ((nav (class . "s bottom"))
+       ,@(map
+          (λ (it)
+            `((a (href . ,(caddr it)))
+              (i ,(cadr it))
+              (span ,(car it))))
+          known-routes))
+      ))))
 
-(define (make-input-item type id label . required?)
-  (case type
-    ('text `((label (class . "field border label"))
-             ((input (name . ,id) ,@(if (not (null? required?)) '((required . "true")) '())))
-             (label ,label)))
-    ('upload `((label (class . "field border label"))
-               ((button (class . "chip circle"))
-                (i "upload")
-                ((input (name . ,id)
-                        (type . "file")
-                        (accept . "image/png image/jpg image/gif image/heic image/heic-sequence")
-                        ,@(if (not (null? required?)) '((required . "true")) '()))))))
-    ('image `((div (class . "row"))
-              ((label (class . "field border label"))
-               ((input (type . "number") (id . ,id) (name . ,id) ,@(if (not (null? required?)) '((required . "true")) '()))
-                (label ,label)))
-              ((button (type . "button") (onClick . ,(str "load_upload_id('" id "')"))) "wybierz " ,label)
-              ))
-    (else (error "unknown type: " type))))
+(define (make-input-item t id label . required?)
+  (let ((t (if (list? t)
+               (list->tuple t)
+               (tuple t))))
+    (tuple-case t
+      ((text)
+       `((label (class . "field border label"))
+         ((input (name . ,id) ,@(if (not (null? required?)) '((required . "true")) '())))
+         (label ,label)))
+      ((upload)
+       `((label (class . "field border label"))
+         ((button (class . "chip circle"))
+          (i "upload")
+          ((input (name . ,id)
+                  (type . "file")
+                  (accept . "image/png image/jpg image/gif image/heic image/heic-sequence")
+                  ,@(if (not (null? required?)) '((required . "true")) '()))))))
+      ((image)
+       `((div (class . "row"))
+         ((label (class . "field border label"))
+          ((input (type . "number") (id . ,id) (name . ,id) ,@(if (not (null? required?)) '((required . "true")) '()))
+           (label ,label)))
+         ((button (type . "button") (onClick . ,(str "load_upload_id('" id "')"))) "wybierz " ,label)
+         ))
+      ((relation tbl)
+       `((div (class . "row"))
+         ((label (class . "field border label"))
+          ((input (type . "number") (id . ,id) (name . ,id) ,@(if (not (null? required?)) '((required . "true")) '()))
+           (label ,label)))
+         ((button (type . "button") (onClick . ,(str "load_relation('" id "', '" tbl "')"))) "wybierz " ,label)
+         ))
+      ((number from to)
+       `((label (class . "field border label"))
+         ((input (name . ,id) (type . "number") (min . ,(str from)) (max . ,(str to))
+                 ,@(if (not (null? required?)) '((required . "true")) '())))
+         (label ,label)))
+      ((bool)
+       `((label (class . "checkbox label"))
+         ((input (name . ,id) (type . "checkbox") ,@(if (not (null? required?)) '((required . "true")) '())))
+         (span ,label)))
+      (else
+       (error "unknown type: " t)))))
 
 (define (make-form action legend lst)
   `((form ("action" . ,action) ("method" . "POST"))
@@ -249,6 +310,13 @@
                              ,(make-list-of tbl t-its)
                              ,@additional)))))
 
+(define route-/ (λ (req)
+                  (r/response
+                   code => 200
+                   headers => '((Content-type . "text/html"))
+                   content => (make-page
+                               `((p "pozdro"))))))
+
 (define route-/roasteries
   (make-page-route "dodaj palarnię"
                    "/new/roastery"
@@ -268,29 +336,110 @@
                    '(script
                      "const form = document.getElementsByTagName('form')[0];"
                      "const input = document.getElementsByTagName('input')[0];"
-                     "form.addEventListener('submit', async (e) => {e.preventDefault(); await fetch('/new/upload', {method: 'PUT', headers:{'Content-type':'application/octet-stream'},body: input.files[0]}); window.location = '/uploads'});")
+                     "form.addEventListener('submit', (e) => {e.preventDefault(); put_image(input.files[0], () => {window.location = '/uploads'});})"
+                     )
                    ))
+
+(define route-/methods
+  (make-page-route "dodaj metodę"
+                   "/new/method"
+                   `((text  "name"  "nazwa" #t)
+                     (image "image" "obrazek")
+                     (text  "notes" "notka"))
+                   'methods
+                   '(name notes)))
+
+(define route-/coffees
+  (make-page-route "dodaj kawkę"
+                   "/new/coffee"
+                   `((text                  "name"        "nazwa"    #t)
+                     ((relation roasteries) "roastery"    "palarnia" #t)
+                     ((number 0 10)         "roast_level" "poziom wypalenia")
+                     (image                 "image"       "zdjęcie")
+                     (text                  "url"         "link")
+                     (text                  "notes"       "notka"))
+                   'coffees
+                   '(name roastery roast_level url notes)))
+
+(define route-/grinders
+  (make-page-route "dodaj młynek"
+                   "/new/grinder"
+                   `((text  "name"  "nazwa" #t)
+                     (image "image" "zdjęcie")
+                     (text  "url"   "link")
+                     (text  "notes" "notka"))
+                   'grinders
+                   '(name url notes)))
+
+(define route-/gear
+  (make-page-route "dodaj machinę (ekspres, drip, kawiarka, ...)"
+                   "/new/gear"
+                   `((text  "name"  "nazwa" #t)
+                     (image "image" "obrazek")
+                     (text  "url"   "link")
+                     (text  "notes" "notka"))
+                   'gear
+                   '(name url notes)))
+
+(define route-/brews
+  (make-page-route "dodaj opinię"
+                   "/new/brew"
+                   `((text                "timestamp"   "data/godzina (unix timestamp)")
+                     ((relation coffees)  "coffee"      "kawka")
+                     ((relation grinders) "grinder"     "młynek")
+                     ((relation methods)  "method"      "metoda")
+                     ((relation gear)     "gear"        "zaparzacz")
+                     (bool                "local_p"     "w domu?")
+                     ((number 0 100)      "grind_level" "klik na młynku")
+                     ((number 0 10)       "rating"      "ocenka")
+                     (image               "image"       "obrazek")
+                     ((number 0 1000)     "dose"        "ilość ziaren kawy (w gramach)")
+                     ((number 0 1000)     "yield"       "ilość wynikowej kawy (w gramach")
+                     (text                "notes"       "notka")
+                     )
+                   'brews
+                   '(timestamp coffee grinder method gear local_p grind_level rating image dose yield notes)))
 
 (define (make-api-responder table . items)
   (λ (req)
     (r/response
      code    => 200
      headers => '((Content-type . "application/json"))
-     content => (json/encode (list->vector (db-get table items))))))
+     content => (json/encode (list->vector (map (λ (l) (zip cons items l))
+                                                (db-get table items)))))))
+
+(define (make-simple-adder constructor redir)
+  (λ (req)
+    (if (not (eq? 'POST (get req 'method 'GET)))
+        (r/response
+         code => 405
+         content => "Method not allowed")
+        (lets ((p (get req 'post-data #n))
+               (p (list->ff p))
+               (p (if (= 0 (string-length (get p 'timestamp "")))
+                      (put p 'timestamp (str (time))) ; lol
+                      p)))
+          (print (ff->list p))
+          ((constructor p) (db))
+          (r/redirect redir)))))
 
 (define app
   (r/make-dispatcher
+   "/"               => route-/
    "/roasteries"     => route-/roasteries
    "/uploads"        => route-/uploads
+   "/methods"        => route-/methods
+   "/coffees"        => route-/coffees
+   "/grinders"       => route-/grinders
+   "/gear"           => route-/gear
+   "/brews"          => route-/brews
 
-   "/new/roastery"   => (λ (req)
-                          (if (not (eq? 'POST (get req 'method 'GET)))
-                              (r/response
-                               code => 405
-                               content => "Method not allowed")
-                              (lets ((p (get req 'post-data #n)))
-                                ((make-roastery (list->ff p)) (db))
-                                (r/redirect "/roasteries"))))
+   "/new/roastery"   => (make-simple-adder make-roastery "/roasteries")
+   "/new/method"     => (make-simple-adder make-method   "/methods")
+   "/new/coffee"     => (make-simple-adder make-coffee   "/coffees")
+   "/new/grinder"    => (make-simple-adder make-grinder  "/grinders")
+   "/new/gear"       => (make-simple-adder make-gear     "/gear")
+   "/new/brew"       => (make-simple-adder make-brew     "/brews")
 
    "/new/upload"   => (λ (req)
                         (if (not (eq? 'PUT (get req 'method 'GET)))
@@ -307,12 +456,23 @@
                                  (s3/execute (db) "INSERT INTO uploads (location, timestamp) VALUES (?, current_timestamp)" (list filename))))
                               (r/response code => 200))))
 
-   "/api/roasteries" => (make-api-responder 'roasteries 'name 'image 'url 'notes)
-   "/api/coffees"    => (make-api-responder 'coffees 'name 'roastery 'roast_level 'image 'url 'additional)
-   "/api/grinders"   => (make-api-responder 'grinders 'name 'image 'url 'additional)
-   "/api/methods"    => (make-api-responder 'methods 'name 'image 'additional)
-   "/api/uploads"    => (make-api-responder 'uploads 'timestamp 'location)
-   "/api/opinions"   => (make-api-responder 'opinions 'timestamp 'coffee 'grinder 'method 'local_p 'grind_level 'rating 'image 'notes)
+   "/api/uploads"    => (make-api-responder 'uploads    'id 'timestamp 'location)
+   "/api/roasteries" => (make-api-responder 'roasteries 'id 'name 'image 'url 'notes)
+   "/api/methods"    => (make-api-responder 'methods    'id 'name 'image 'notes)
+   "/api/coffees"    => (make-api-responder 'coffees    'id 'name 'roastery 'roast_level 'image 'url 'notes)
+   "/api/grinders"   => (make-api-responder 'grinders   'id 'name 'image 'url 'notes)
+   "/api/gear"       => (make-api-responder 'gear       'id 'name 'url 'image 'notes)
+   "/api/brews"      => (make-api-responder 'brew       'id 'timestamp 'coffee 'grinder 'method 'gear 'local_p 'grind_level 'rating 'image 'dose 'yield 'notes)
+
+   "m/^\\/uploads\\/[0-9]+$/" => (λ (r)
+                                   (let ((id (string->number (last ((string->regex "c/\\//") (get r 'path 'bug)) 0))))
+                                     (let ((res (execute* (db) "SELECT location FROM uploads WHERE id = ?" (list id))))
+                                       (if (null? res)
+                                           (r/response code => 404)
+                                           (r/response
+                                            code    => 200
+                                            headers => '((Content-type . "image/jpg"))
+                                            content => (file->list (caar res)))))))
    "m/^\\/static/"   => (λ (r) (r/static-dispatcher "static" "/static" r))
    ))
 
--- static/app.js
+++ static/app.js
@@ -1,13 +1,155 @@
 let image_selected = _ => true;
 
-function load_upload_id(id) {
-  const dlg = document.getElementById('choose_upload_dialog');
-  const it = document.getElementById(id);
-  console.log(it)
-  dlg.show();
-  image_selected = (img) => {
-    console.log(img)
-    it.value = img;
-    dlg.close();
+function E(t, kv) {
+  const el = document.createElement(t);
+  for (let k of Object.keys(kv || {})) {
+    el[k] = kv[k];
   }
+  return el;
+}
+
+function make_dialog(title, cont) {
+  const dlg = document.createElement('dialog');
+  const h5 = document.createElement('h5');
+  const art = document.createElement('article');
+  h5.innerHTML = title;
+  dlg.appendChild(h5);
+  dlg.appendChild(art);
+  cont(art);
+  return dlg;
+}
+
+// `((article (class . "no-padding border"))
+//   ((img (class . "responsive medium") (src . ,(str (car it))) (loading . "lazy")))
+//   ((div (class . "absolute bottom left right padding bottom-shadow white-text"))
+//    (nav
+//     ((button (class . "circle transparent") (type . "button") (onClick . ,(str "image_selected('" (str (cadr it)) "')")))
+//      (i "add"))))))
+
+function make_img(icont, bcont, title) {
+  const el = document.createElement('article');
+  el.classList = "no-padding border";
+
+  const img = document.createElement('img');
+  img.classList = "responsive medium";
+  img.loading = "lazy";
+  icont(img);
+  el.appendChild(img);
+
+  const div = document.createElement('div');
+  div.classList = "absolute bottom left right padding bottom-shadow white-text";
+  el.appendChild(div);
+
+  const nav = document.createElement('nav');
+  div.appendChild(nav);
+
+  // const what = document.createElement('div');
+  // what.classList = 'max';
+  // nav.appendChild(what);
+
+  if (title != undefined)
+    nav.appendChild(E('p', {innerHTML: title}))
+
+  const button = document.createElement('button');
+  button.classList = "circle transparent";
+  button.type = "button";
+  bcont(button);
+  nav.appendChild(button);
+
+  const i = document.createElement('i');
+  i.innerHTML = "check";
+  button.appendChild(i);
+
+  return el;
+}
+
+function load_relation(id, table) {
+  const target = document.getElementById(id);
+  const refresh = () => {
+    const dlg = make_dialog(`wybierz ${table}`, body => {
+      fetch(`/api/${table}`).then(r => r.json()).then(its => {
+        for (let it of its.reverse()) {
+          const el = make_img(i => {
+            i.src = `/uploads/${it['image']}`;
+          }, b => {
+            b.addEventListener('click', () => {
+              target.value = it['id'];
+              dlg.close();
+            });
+          }, it['name']);
+          body.appendChild(el);
+        }
+      });
+    });
+    document.body.appendChild(dlg);
+    dlg.showModal();
+  }
+  refresh();
+}
+
+function load_upload_id(id) {
+  const it = document.getElementById(id);
+  const refresh = () => {
+    const dlg = make_dialog('wybierz obrazek', body => {
+      fetch('/api/uploads').then(r => r.json()).then(ups => {
+        const rb = E('button', {classList: 'chip circle'});
+        rb.appendChild(E('i', {innerHTML: 'refresh'}));
+        rb.addEventListener('click', () => {
+          dlg.close();
+          refresh();
+        });
+
+        const uploader = make_image_uploader(() => {
+          dlg.close();
+          refresh();
+        });
+
+        uploader.appendChild(rb);
+        body.appendChild(uploader);
+
+        for (let up of ups.reverse()) {
+          const el = make_img(i => {
+            i.src = up['location']
+          }, b => {
+            b.addEventListener('click', () => {
+              it.value = up['id'];
+              dlg.close();
+            });
+          });
+          body.appendChild(el);
+        }
+      })
+    });
+    document.body.appendChild(dlg);
+    dlg.showModal();
+  }
+  refresh();
+}
+
+function put_image(file, cont) {
+  fetch('/new/upload', {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/octet-stream'
+    },
+    body: file
+  }).then(() => cont());
+}
+
+function make_image_uploader(after_upload) {
+  const el = E('span');
+  const label = E('label', {classList: 'field border label'});
+  const btn = E('button', {classList: 'chip circle'});
+  btn.appendChild(E('i', {innerHTML: 'upload'}));
+  const input = E('input', {type: "file",
+                            accept: "image/png image/jpg image/gif image/heic image/heic-sequence",
+                            required: "true"});
+  input.addEventListener('change', () => {
+    put_image(input.files[0], after_upload);
+  });
+
+  btn.appendChild(input);
+  el.appendChild(label);
+  el.appendChild(btn);
+  return el;
 }
