2ced53046c3c2eb7ae1a962e41739d3c62e89344
Krzysztof Michałczyk <kpm@krzysckh.org>
2025-12-25 20:19:23 +0100

    use current tz offset as default *tz-offset*

--- moka-config.scm
+++ moka-config.scm
@@ -1,4 +1,9 @@
 (define *db-file* "moka.db")
 (define *uploads-dir* "static/uploads")
 (define *port* 8081) ; port for app served via fastcgi
-(define *tz-offset* 2)
+
+(define *tz-offset*
+  (lets ((r w (popen "date +%z"))
+         (v (/ (read (car* (force-ll (lines r)))) 100)))
+    (map close-port (list r w))
+    v))
